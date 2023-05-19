7aa05ff28b2d267f9262199afd88df1bc5779799
TanvirAhmed41 <santoahmed225@gmail.com>
2023-05-20 04:41:08 +0600

    fixed my toys and delete items

--- src/Home/MyToys/MyToys.jsx
+++ src/Home/MyToys/MyToys.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../AuthProvider/AuthProvider";
+import SingleMyToys from "./SingleMyToys";
 
 const MyToys = () => {
     const {user} = useContext(AuthContext)
@@ -12,12 +13,48 @@ const MyToys = () => {
             .then(data => {
                 setAnimalToys(data);
             })
-    }, [url])
+    }, [url]);
+
+    const handleDelete = id => {
+        const proceed = confirm('Are You sure you want to delete');
+        if (proceed) {
+            fetch(`http://localhost:5000/allToys/${id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    if (data.deletedCount > 0) {
+                        alert('deleted successful');
+                        const remaining = animalToys.filter(toys => toys._id !== id);
+                        setAnimalToys(remaining);
+                    }
+                })
+        }
+    }
+
+
     return (
         <div>
+            <table className="table table-compact w-full">
+          <thead >
+            <tr className="py-5">
+              <th className="py-5">Name</th>
+              <th>Seller </th>
+              <th>Sub Category</th>
+              <th>Price</th>
+              <th>Details</th>
+            </tr>
+          </thead>
+          <tbody className="py-5">
             
+                {
+                    animalToys &&
+                    animalToys.map((toy) =><SingleMyToys key={toy._id} handleDelete={handleDelete} toy={toy}></SingleMyToys>)
+                }
+          </tbody>
+        </table>
         </div>
     );
 };
-
 export default MyToys;
---
+++ src/Home/MyToys/SingleMyToys.jsx
@@ -0,0 +1,49 @@
+const SingleMyToys = ({ toy, handleDelete }) => {
+  const {
+    name,
+    rating,
+    pictureURL,
+    description,
+    price,
+    sellerEmail,
+    sellerName,
+    subCategory,
+    _id,
+  } = toy;
+  return (
+    <tr>
+      <th>
+        <label>
+          <button
+            onClick={() => handleDelete(_id)}
+            className="btn btn-circle btn-outline hover:bg-green-200]"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+        </label>
+      </th>
+      <th>{name}</th>
+      <th>{sellerName}</th>
+      <th>{subCategory}</th>
+      <th>{price}</th>
+      <th>
+        <button className="btn btn-primary">View Details</button>
+      </th>
+    </tr>
+  );
+};
+
+export default SingleMyToys;
