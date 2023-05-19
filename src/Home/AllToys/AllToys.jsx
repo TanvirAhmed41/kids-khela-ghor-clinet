c086c7a32047544d444108ba4d0a5912c7237862
TanvirAhmed41 <santoahmed225@gmail.com>
2023-05-20 05:26:02 +0600

    toy add and single toys added

--- src/Home/AllToys/AllToys.jsx
+++ src/Home/AllToys/AllToys.jsx
@@ -1,25 +1,43 @@
 import { useEffect, useState } from "react";
 import AnimalTable from "../AnimalTable/AnimalTable";
 
-
 const AllToys = () => {
-  const [animalToys,setAnimalToys] = useState([]);
+  const [animalToys, setAnimalToys] = useState([]);
+  const [searchAnimal, setSearchAnimal] = useState([]);
   console.log(animalToys);
-  useEffect (()=>{
-    fetch('http://localhost:5000/allToys')
-    .then(res => res.json())
-    .then(data => setAnimalToys(data))
-  },[])
+
+  useEffect(() => {
+    fetch("http://localhost:5000/allToys")
+      .then((res) => res.json())
+      .then((data) => setAnimalToys(data));
+  }, []);
+
+  const handleSearch = () => {
+    fetch(`http://localhost:5000/toySearchByName/${searchAnimal}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setAnimalToys(data);
+      });
+  };
 
   return (
     <div>
       <div className="overflow-x-auto">
         <div className="flex justify-center items-center">
-            <input type="search" placeholder="search" className="w-1/2 p-3 my-5 rounded bg-white " name="" id="" />
-            <button className="btn btn-primary">Search</button>
+          <input
+            onChange={(event) => setSearchAnimal(event.target.value)}
+            type="search"
+            placeholder="search"
+            className="w-1/2 p-3 my-5 rounded bg-white "
+            name=""
+            id=""
+          />
+          <button onClick={handleSearch} className="btn btn-primary">
+            Search
+          </button>
         </div>
         <table className="table table-compact w-full">
-          <thead >
+          <thead>
             <tr className="py-5">
               <th className="py-5">Name</th>
               <th>Seller </th>
@@ -29,9 +47,9 @@ const AllToys = () => {
             </tr>
           </thead>
           <tbody className="py-5">
-                {
-                    animalToys.map((toy) =><AnimalTable key={toy._id} toy={toy}></AnimalTable>)
-                }
+            {animalToys.map((toy) => (
+              <AnimalTable key={toy._id} toy={toy}></AnimalTable>
+            ))}
           </tbody>
         </table>
       </div>
